ae24f74ba4022416e09cd88fb6503cc9db0f8e0e
Administrator <1846201952@qq.com>
2021-01-10 15:27:48 +0800

    1、继承父类

---
+++ src/Student.java
@@ -0,0 +1,7 @@
+public class Student extends User{
+    private String phone;
+    private int year;
+
+    public Student() {
+    }
+}
---
+++ src/text.java
@@ -0,0 +1,19 @@
+import java.io.File;
+
+public class text {
+    public static void main(String args[]) {
+        File file = new File("F:\\qin\\2");
+        men(file);
+    }
+    public static void men(File file) {
+        if (file.isDirectory()) {
+            File files[] = file.listFiles();
+            for (File f: files) {
+                men(f);
+            }
+        }
+            else{
+                System.out.println(file.getName());
+            }
+    }
+}
