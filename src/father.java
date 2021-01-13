26bec380ad50d895d1112aeaad1f92af87ff8543
panguanliang <1846201952@qq.com>
2021-01-13 08:11:04 +0800

    第五天

--- src/father.java
+++
@@ -1,8 +0,0 @@
-public class father {
-    String ID;
-    String password;
-    public father(String ID,String password){
-        this.ID=ID;
-        this.password=password;
-    }
-}
--- src/superman.java
+++
@@ -1,8 +0,0 @@
-public class superman{
-    public static void main(String[] args) {
-        son son01=new son("men","17878890425");
-        son son02=new son("men","12345678965");
-        System.out.println(son01);
-        System.out.println(son02);
-    }
-}
